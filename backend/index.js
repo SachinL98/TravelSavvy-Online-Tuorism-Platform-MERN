2280d766b68fc496b2536e48725f7c40bcd8ee44
Pasindu Shyaminda <it21016202@my.sliit.lk>
2023-05-04 19:02:59 +0530

    auth and event crud created

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,14 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors  = require('cors');
 require("dotenv").config();
+const userRoutes = require('./routes/user')
 
 const port = process.env.PORT || 8080
 const mongo_url = process.env.MONGO_URL;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use("/user", userRoutes);
 
 mongoose.connect(mongo_url, {});
 const connection = mongoose.connection;
